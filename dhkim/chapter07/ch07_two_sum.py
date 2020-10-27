8365a6cf66d587869a1a1a333fc2276a032311be
dhkdn9192 <dhkdn9192@naver.com>
2020-10-28 07:45:53 +0900

    [dhkim] ch07 두 수의 합 - O(n) 해결법 추가, README 추가

--- dhkim/chapter07/ch07_two_sum.py
+++ dhkim/chapter07/ch07_two_sum.py
@@ -6,8 +6,13 @@ https://leetcode.com/problems/two-sum/
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
+    def twoSum_1(self, nums: List[int], target: int) -> List[int]:
+        """
+        O(n^2) 나이브한 해결
+        :param nums:
+        :param target:
+        :return:
+        """
         for i in range(len(nums)):
 
             for j in range(i + 1, len(nums)):
@@ -17,3 +22,19 @@ class Solution:
                 if sum_value == target:
                     return [i, j]
 
+    def twoSum_2(self, nums: List[int], target: int) -> List[int]:
+        """
+        O(n) 해결법. 숫자->인덱스 딕셔너리 이용
+        :param nums:
+        :param target:
+        :return:
+        """
+        num2idx_dict = dict()
+
+        for idx, num in enumerate(nums):
+            diff = target - num
+
+            if diff in num2idx_dict.keys():
+                return [num2idx_dict[diff], idx]
+
+            num2idx_dict[num] = idx
